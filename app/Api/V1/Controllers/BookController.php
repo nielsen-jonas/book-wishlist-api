d0b22a36e8d546eb0f1d0c790bd5205ce6b1d46d
Jonas Nielsen <dev@jonas92.com>
2016-11-26 20:44:50 +0100

    Implement in BookController index and store methods

--- app/Api/V1/Controllers/BookController.php
+++ app/Api/V1/Controllers/BookController.php
@@ -11,4 +11,39 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
     use Helpers; 
+
+    /*
+     * Get the user starting from the token, and then return all the related books as output.
+     */
+    public function index()
+    {
+        $currentUser = JWTAuth::parseToken()->authenticate();
+        return $currentUser
+            ->books()
+            ->orderBy('created_at', 'DESC')
+            ->get()
+            ->toArray();
+    }
+
+    /*
+     * Get the current user with the JWTAuth method, parseToken()->authenticate(). The create the instance and save the relationship with the save method.
+     *
+     * If everything goes right, return a 'Created 201'. Otherwise return a custom 500 error.
+     * Not just the body: Headers are tweaked to follow RESTful standards.
+     */
+    public function store(Request $request)
+    {
+        $currentUser = JWTAuth::parseToken()->authenticate();
+
+        $book = new Book;
+
+        $book->title = $request->get('title');
+        $book->author_name = $request->get('author_name');
+        $book->pages_count = $request->get('pages_count');
+
+        if($currentUser->books()->save($book))
+            return $this->response->created();
+        else
+            return $this->response->error('could_not_create_book', 500);
+    }
 }
