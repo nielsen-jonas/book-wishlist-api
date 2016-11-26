430c3f391c6e9ecdbf67aa64ddc75ad650989f3a
Jonas Nielsen <dev@jonas92.com>
2016-11-26 20:24:45 +0100

    Add use directives and helpers trait for the BookController

--- app/Api/V1/Controllers/BookController.php
+++ app/Api/V1/Controllers/BookController.php
@@ -1,10 +1,14 @@
 <?php
 
+use JWTAuth;
+use App\Book;
+use Dingo\Api\Routing\Helpers;
+
 namespace App\Api\V1\Controllers;
 
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    use Helpers; 
 }
